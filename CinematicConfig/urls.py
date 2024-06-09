02bb152a1c2a2d7e009c8be31241d1d8a59643c5
Oscardk <dauren2050@yandex.ru>
2024-06-09 17:40:33 +0500

    finish project

--- CinematicConfig/urls.py
+++ CinematicConfig/urls.py
@@ -1,22 +1,9 @@
-# urls.py
+# CinematicConfig/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework import permissions
-from cinema.views import MovieViewSet, CinemaViewSet, RoomViewSet, SeatViewSet, ShowtimeViewSet, TicketViewSet, FeedbackViewSet, DiscountViewSet, PurchaseHistoryViewSet
-
-router = DefaultRouter()
-router.register(r'movies', MovieViewSet)
-router.register(r'cinemas', CinemaViewSet)
-router.register(r'rooms', RoomViewSet)
-router.register(r'seats', SeatViewSet)
-router.register(r'showtimes', ShowtimeViewSet)
-router.register(r'tickets', TicketViewSet)
-router.register(r'feedback', FeedbackViewSet)
-router.register(r'discounts', DiscountViewSet)
-router.register(r'purchase_histories', PurchaseHistoryViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,7 +20,48 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('cinema.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+# # urls.py
+# from django.contrib import admin
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+# from rest_framework import permissions
+# from cinema.views import MovieViewSet, CinemaViewSet, RoomViewSet, SeatViewSet, ShowtimeViewSet, TicketViewSet, FeedbackViewSet, DiscountViewSet, PurchaseHistoryViewSet
+
+# router = DefaultRouter()
+# router.register(r'movies', MovieViewSet)
+# router.register(r'cinemas', CinemaViewSet)
+# router.register(r'rooms', RoomViewSet)
+# router.register(r'seats', SeatViewSet)
+# router.register(r'showtimes', ShowtimeViewSet)
+# router.register(r'tickets', TicketViewSet)
+# router.register(r'feedback', FeedbackViewSet)
+# router.register(r'discounts', DiscountViewSet)
+# router.register(r'purchase_histories', PurchaseHistoryViewSet)
+
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Cinema API",
+#         default_version='v1',
+#         description="API documentation for Cinema project",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@cinema.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include(router.urls)),
+#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+# ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,40 +1,75 @@
-# views.py
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Movie, Cinema, Room, Seat, Showtime, Ticket, Feedback, Discount, PurchaseHistory
 from .serializers import MovieSerializer, CinemaSerializer, RoomSerializer, SeatSerializer, ShowtimeSerializer, TicketSerializer, FeedbackSerializer, DiscountSerializer, PurchaseHistorySerializer
 
-class MovieViewSet(viewsets.ModelViewSet):
+class MovieListCreateView(generics.ListCreateAPIView):
+    queryset = Movie.objects.filter(is_active=True)
+    serializer_class = MovieSerializer
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-class CinemaViewSet(viewsets.ModelViewSet):
+class CinemaListCreateView(generics.ListCreateAPIView):
     queryset = Cinema.objects.all()
     serializer_class = CinemaSerializer
 
-class RoomViewSet(viewsets.ModelViewSet):
+class CinemaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+class RoomListCreateView(generics.ListCreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
-class SeatViewSet(viewsets.ModelViewSet):
+class RoomRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class SeatListCreateView(generics.ListCreateAPIView):
     queryset = Seat.objects.all()
     serializer_class = SeatSerializer
 
-class ShowtimeViewSet(viewsets.ModelViewSet):
+class SeatRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class ShowtimeListCreateView(generics.ListCreateAPIView):
     queryset = Showtime.objects.all()
     serializer_class = ShowtimeSerializer
 
-class TicketViewSet(viewsets.ModelViewSet):
+class ShowtimeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Showtime.objects.all()
+    serializer_class = ShowtimeSerializer
+
+class TicketListCreateView(generics.ListCreateAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
-class FeedbackViewSet(viewsets.ModelViewSet):
+class TicketRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+class FeedbackListCreateView(generics.ListCreateAPIView):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
 
-class DiscountViewSet(viewsets.ModelViewSet):
+class FeedbackRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+
+class DiscountListCreateView(generics.ListCreateAPIView):
     queryset = Discount.objects.all()
     serializer_class = DiscountSerializer
 
-class PurchaseHistoryViewSet(viewsets.ModelViewSet):
+class DiscountRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = DiscountSerializer
+
+class PurchaseHistoryListCreateView(generics.ListCreateAPIView):
+    queryset = PurchaseHistory.objects.all()
+    serializer_class = PurchaseHistorySerializer
+
+class PurchaseHistoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = PurchaseHistory.objects.all()
     serializer_class = PurchaseHistorySerializer
